5749ce7363083b07a3b017c6fd6d8c7b40bef272
jkarlos000 <jc.ulloa@jkarlos.info>
2020-03-25 09:55:51 -0400

    ÂOtras formas de decaración de MAPS

--- main.go
+++ main.go
@@ -4,15 +4,6 @@ import "fmt"
 
 func main() {
 	// Diccionarios (asi se conoce en Python) de Go lang (MAP)
-	moons := make(map[string]string)
-
-	// Asignamos los valores
-	moons["Earth"] = "Moon"
-	moons["Jupiter"] = "Europa"
-	moons["Saturn"] = "Titan"
-
-	fmt.Println(moons)
-
-	delete(moons, "Saturn")
+	moons := map[string]string{"Earth": "Moon", "Jupiter": "Europa", "Saturn": "Titan"}
 	fmt.Println(moons)
 }
