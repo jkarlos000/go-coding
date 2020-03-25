37d34bafda3d089f31a258f77f07beafd10c2a38
jkarlos000 <jc.ulloa@jkarlos.info>
2020-03-24 23:58:04 -0400

    slices and make

--- main.go
+++ main.go
@@ -3,11 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	/*var frutas [2]string
-	frutas[0] = "manzana"
-	frutas[1] = "platano"*/
-	frutas := [2]string{"manzana", "platano"}
-	ids := []int{1,3,5,7,9}
-	fmt.Println(frutas)
-	fmt.Println(ids)
+	alphabet := []string{"a","b","c","d","e"}
+	fmt.Println(alphabet)
+
+	// make (type, len, cap)
+	s := make([]int, 5, 10)
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s),s)
 }
