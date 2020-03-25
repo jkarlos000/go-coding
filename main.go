7f2950537bbe7894c6f6a51c191e01c6412231e7
jkarlos000 <jc.ulloa@jkarlos.info>
2020-03-25 00:56:33 -0400

    Ciclos, unicamente existe el FOR, asi que a practicar para volverlo un WHILE o FOREACH

--- main.go
+++ main.go
@@ -3,15 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	galaxy := "M87"
-	switch galaxy {
-	case "Mliky Way":
-		fmt.Printf("Gaalaxy name is 'Milky Way'\n")
-	case "Andromeda":
-		fmt.Printf("Galaxy name is 'Andromeda'\n")
-	case "M87":
-		fmt.Printf("Galaxy name is 'M87'\n")
-	default:
-		fmt.Printf("Don't exits a Galaxy")
+	for i := 1; i <= 100; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if i%3 == 0 {
+			fmt.Println("Fizz")
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
+		} else {
+			fmt.Println(i)
+		}
 	}
 }
