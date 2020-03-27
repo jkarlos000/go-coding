de5e05f20536155c5e9902e0baab7e3eda112194
jkarlos000 <jc.ulloa@jkarlos.info>
2020-03-26 22:30:01 -0400

    Funciones con multiples parametros de entrada, usar en consultas SQL

--- main.go
+++ main.go
@@ -1,20 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-func leftRotation(a []int, size int, rotation int) []int {
-	var newArray []int
-	for i := 0; i < rotation; i++{
-		newArray = a[1:size]
-		newArray = append(newArray, a[0])
-		a = newArray
-	}
-	return a
+func getName(names ...string) string {
+	return strings.Join(names, " ")
 }
 
 func main() {
-	a := []int{1,2,3,4,5,6}
-	fmt.Printf("input: %+v\n", a)
-	rotation := 4
-	fmt.Printf("output: %+v\n", leftRotation(a, len(a), rotation))
+	fmt.Println(getName("Claude", "Debussy"))
+	fmt.Println(getName("Ravel"))
+	fmt.Println(getName(""))
 }
