7356d546692cd5dc08ae56b3958b289536acb9e3
jkarlos000 <jc.ulloa@jkarlos.info>
2020-03-26 23:33:23 -0400

    Canales con Selectores, interesante como funciona investigar mas al respecto

--- main.go
+++ main.go
@@ -2,48 +2,34 @@ package main
 
 import "fmt"
 
-// Recursiva
-func fibor(n int) int {
-	if n <= 1 {
-		return n
-	}
-	return fibor(n-1) + fibor(n - 2)
-}
-
-// iterative using closure
-func fiboi() func() int {
+func fibonacci(c, quit chan int) {
+	fmt.Println("Serie Fibonacci")
 	x, y := 0, 1
-	return func() int {
-		r := x
-		x, y = y, x+y
-		return r
+	for {
+		fmt.Printf("x = %d\n",x)
+		select {
+		case c <- x:
+			x, y = y, x+y
+		case quit_value := <-quit:
+			fmt.Printf("quit_value = %d\n", quit_value)
+			return
+		default:
+			fmt.Println("Sin actividad")
+		}
 	}
-}
-
-func fiboc(n int, c chan int) {
-	x, y := 0, 1
-	for i := 0; i < n; i++ {
-		c <- x
-		x, y = y, x+y
-	}
-	close(c)
 }
 
 func main() {
-	n := 10
-	for i := 0; i < n; i++ {
-		fmt.Printf("%d ", fibor(i))
-	}
-	fmt.Println()
-	next_fibo := fiboi()
-	for i := 0; i < n; i++ {
-		fmt.Printf("%d ", next_fibo())
-	}
-	fmt.Println()
-	c := make(chan int, n)
-	go fiboc(cap(c), c)
-	for i := range c{
-		fmt.Printf("%d ", i)
-	}
-	fmt.Println()
+	c := make(chan int)
+	q := make(chan int)
+	go func() {
+		fmt.Println("GoRoutine Iniciando")
+		for i := 0; i < 10; i++ {
+			value := <-c
+			fmt.Printf("received %d\n", value)
+		}
+		q <- 999
+	}()
+	fmt.Println("Ejecutando fibonacci()")
+	fibonacci(c,q)
 }
