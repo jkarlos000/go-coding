9f30b9a5c6c87121fc991dbf637053549a032942
jkarlos000 <jc.ulloa@jkarlos.info>
2020-03-27 00:03:22 -0400

    Usando Difer para retrasar la salida de una funcion, pero funciona al revez las N llamadas LIFO-Pila

--- main.go
+++ main.go
@@ -1,37 +1,10 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
-func worker(id int, jobs <-chan int, results chan <- int)  {
-	for j := range jobs {
-		fmt.Println("worker", id, "inicio el trabajo:",j)
-		time.Sleep(1*time.Millisecond)
-		fmt.Println("worker",id,"termino el trabajo",j)
-		results <- j*2
-	}
-}
+import "fmt"
 
 func main() {
-	// Dos canales - se envía el trabajo y se recolecta el resultado
-	jobs := make(chan int, 10)
-	results := make(chan int, 100)
-
-	// Iniciamos primero con 3 trabajadores, inicialmente bloqueados debido a que no hay trabajos hechos
-	for w:=1; w <= 3; w++ {
-		go worker(w, jobs, results)
+	for i:=0; i<5; i++ {
+		defer fmt.Println(i)
 	}
-
-	// Aquí enviaremos 5 'trabajos' y también 'cerraremos' el canal indicando que todos los trabajos están hechos
-	for j := 1; j <= 5; j++ {
-		jobs <- j
-	}
-	close(jobs)
-	//time.Sleep(200*time.Millisecond)
-	// recolectamos todos los resultados del trabajo
-	for a:=1; a<=5; a++ {
-		<-results
-	}
+	fmt.Println("main")
 }
