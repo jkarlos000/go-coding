a8df906e7902ba88a6b2205048f08575b36f820b
jkarlos000 <jc.ulloa@jkarlos.info>
2020-03-26 22:44:33 -0400

    Declarando Canales en GO, para compartir memoria entre GoRoutine

--- main.go
+++ main.go
@@ -1,23 +1,10 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
-func sleep_250()  {
-	fmt.Println("sleep_250")
-	time.Sleep(250*time.Millisecond)
-}
-
-func sleep_500() {
-	fmt.Println("sleep_500")
-	time.Sleep(500*time.Millisecond)
-}
+import "fmt"
 
 func main() {
-	go sleep_250()
-	go sleep_500()
-	time.Sleep(1000 * time.Millisecond)
-	fmt.Println("Main")
+	var my_int_Channel chan int
+	my_string_Channel := make(chan string)
+	fmt.Printf("The type of my_int_Channel is '%T'\n", my_int_Channel)
+	fmt.Printf("The type of my_string_Channel is '%T'\n", my_string_Channel)
 }
