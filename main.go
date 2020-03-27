fa6e0eb0664f5a7d1f5f82359930f2aaf8a94ace
jkarlos000 <jc.ulloa@jkarlos.info>
2020-03-26 22:26:15 -0400

    Slices y Matrices rotacion a la izquierda

--- main.go
+++ main.go
@@ -1,120 +1,20 @@
 package main
 
-import (
-	"errors"
-	"html/template"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"regexp"
-)
+import "fmt"
 
-type Page struct {
-	Title string
-	Body []byte
-}
-var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
-var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
-
-func (p *Page) save() error {
-	filename := p.Title+".txt"
-	return ioutil.WriteFile(filename, p.Body, 0600)
-}
-
-func loadPage(title string) (*Page, error) {
-	filename := title+".txt"
-	body, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
+func leftRotation(a []int, size int, rotation int) []int {
+	var newArray []int
+	for i := 0; i < rotation; i++{
+		newArray = a[1:size]
+		newArray = append(newArray, a[0])
+		a = newArray
 	}
-	return &Page{
-		Title: title,
-		Body:  body,
-	}, nil
-}
-
-func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		m:= validPath.FindStringSubmatch(r.URL.Path)
-		if m == nil {
-			http.NotFound(w,r)
-			return
-		}
-		fn(w,r,m[2])
-	}
-}
-
-func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
-	/*title, err := getTitle(w, r)
-	if err != nil {
-		return
-	}*/
-	p, err := loadPage(title)
-	if err != nil {
-		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
-		return
-	}
-	renderTemplate(w, "view", p)
-}
-
-func editHandler(w http.ResponseWriter, r *http.Request, title string) {
-	/*title, err := getTitle(w, r)
-	if err != nil {
-		return
-	}*/
-	p, err := loadPage(title)
-	if err != nil {
-		p = &Page{
-			Title: title,
-		}
-	}
-	renderTemplate(w,"edit", p)
-}
-
-func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
-	/*title, err := getTitle(w, r)
-	if err != nil {
-		return
-	}*/
-	body := r.FormValue("body")
-	p := &Page{
-		Title: title,
-		Body:  []byte(body),
-	}
-	err := p.save()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	http.Redirect(w,r,"/view/"+title, http.StatusFound)
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	// t, err := template.ParseFiles(tmpl+".html")
-	err := templates.ExecuteTemplate(w, tmpl+".html", p)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	/*err = t.Execute(w, p)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}*/
-}
-
-func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
-	m := validPath.FindStringSubmatch(r.URL.Path)
-	if m == nil {
-		http.NotFound(w, r)
-		return "", errors.New("Invalid Page Title")
-	}
-	return m[2], nil // The title is the second subexpression.
+	return a
 }
 
 func main() {
-	http.HandleFunc("/view/", makeHandler(viewHandler))
-	http.HandleFunc("/edit/", makeHandler(editHandler))
-	http.HandleFunc("/save/", makeHandler(saveHandler))
-	println("Iniciando servidor....")
-	log.Fatal(http.ListenAndServe(":3000", nil))
+	a := []int{1,2,3,4,5,6}
+	fmt.Printf("input: %+v\n", a)
+	rotation := 4
+	fmt.Printf("output: %+v\n", leftRotation(a, len(a), rotation))
 }
