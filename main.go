c418dd7a3bc6bb8d4bac813893e1a3e8043dce15
jkarlos000 <jc.ulloa@jkarlos.info>
2020-03-26 23:50:45 -0400

    GoRoutine creando Trabajadores que pelean por prioridades de ejcucion, interesante

--- main.go
+++ main.go
@@ -1,35 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func fibonacci(c, quit chan int) {
-	fmt.Println("Serie Fibonacci")
-	x, y := 0, 1
-	for {
-		fmt.Printf("x = %d\n",x)
-		select {
-		case c <- x:
-			x, y = y, x+y
-		case quit_value := <-quit:
-			fmt.Printf("quit_value = %d\n", quit_value)
-			return
-		default:
-			fmt.Println("Sin actividad")
-		}
+func worker(id int, jobs <-chan int, results chan <- int)  {
+	for j := range jobs {
+		fmt.Println("worker", id, "inicio el trabajo:",j)
+		time.Sleep(1*time.Millisecond)
+		fmt.Println("worker",id,"termino el trabajo",j)
+		results <- j*2
 	}
 }
 
 func main() {
-	c := make(chan int)
-	q := make(chan int)
-	go func() {
-		fmt.Println("GoRoutine Iniciando")
-		for i := 0; i < 10; i++ {
-			value := <-c
-			fmt.Printf("received %d\n", value)
-		}
-		q <- 999
-	}()
-	fmt.Println("Ejecutando fibonacci()")
-	fibonacci(c,q)
+	// Dos canales - se envía el trabajo y se recolecta el resultado
+	jobs := make(chan int, 10)
+	results := make(chan int, 100)
+
+	// Iniciamos primero con 3 trabajadores, inicialmente bloqueados debido a que no hay trabajos hechos
+	for w:=1; w <= 3; w++ {
+		go worker(w, jobs, results)
+	}
+
+	// Aquí enviaremos 5 'trabajos' y también 'cerraremos' el canal indicando que todos los trabajos están hechos
+	for j := 1; j <= 5; j++ {
+		jobs <- j
+	}
+	close(jobs)
+	//time.Sleep(200*time.Millisecond)
+	// recolectamos todos los resultados del trabajo
+	for a:=1; a<=5; a++ {
+		<-results
+	}
 }
