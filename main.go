047ffa70b05be7495d6bdd75c54852db2ba6ad61
jkarlos000 <jc.ulloa@jkarlos.info>
2020-03-28 14:20:39 -0400

    Listas enlazadas, usando Punteros y Nodos, Interesante para Pilas, Colas, Grafos

--- main.go
+++ main.go
@@ -1,29 +1,108 @@
 package main
 
-import (
-	"database/sql"
-	"fmt"
-	"strconv"
+import "fmt"
 
-	_ "github.com/mattn/go-sqlite3"
-)
+type Chichis struct {
+	next *Chichis
+	apodo string
+	field string
+	edad int
+}
+
+type ChichitosList struct {
+	head *Chichis
+	name string
+}
+
+func createChichitosList(n string) *ChichitosList {
+	return &ChichitosList{
+		name: n,
+	}
+}
+
+func (l_list *ChichitosList) addChichis(a string, f string, y int) error {
+	fmt.Printf("Agregando Chichito %s %s %d\n", a,f,y)
+	l := &Chichis{
+		apodo: a,
+		field: f,
+		edad:  y,
+	}
+	if l_list.head == nil {
+		l_list.head = l
+	} else {
+		currentNode := l_list.head
+		for currentNode.next != nil {
+			currentNode = currentNode.next
+		}
+		currentNode.next = l
+	}
+	return nil
+}
+
+func (l_list *ChichitosList) deleteChichis(a string, f string, y int) error {
+	fmt.Printf("Eliminando Chichito: %s %s %d\n", a, f, y)
+
+	currentNode := l_list.head
+	if currentNode == nil {
+		fmt.Println("No hay Chichitos!!!! ayuda!")
+		return nil
+	}
+
+	// head
+	if currentNode.apodo == a && currentNode.field == f && currentNode.edad == y {
+		if currentNode == l_list.head {
+			l_list.head = currentNode.next
+		}
+		return nil
+	}
+
+	// others
+
+	fmt.Printf("*currentNode %+v\n", *currentNode)
+	for currentNode.next != nil  {
+		fmt.Printf("*currentNode.next: %+v\n", *currentNode.next)
+		next := currentNode.next
+		if next.apodo == a && next.field == f && next.edad == y {
+			fmt.Println("El chichito buscado fue encontrado en el enlace siguiente!")
+			currentNode.next = next.next
+			break
+		}
+		currentNode = currentNode.next
+	}
+	return nil
+}
+
+func (l_list *ChichitosList) showAllChichitos() error {
+	fmt.Printf("\nLista actual de Chichitos:\n")
+	fmt.Println("----------------------")
+	currentNode := l_list.head
+	if currentNode == nil {
+		fmt.Println("Lista de Chichitos Vacía")
+		return nil
+	}
+	fmt.Printf("%+v\n", *currentNode)
+	for currentNode.next != nil  {
+		currentNode = currentNode.next
+		fmt.Printf("%+v\n", *currentNode)
+	}
+	fmt.Println("----------------------")
+	return nil
+}
 
 func main() {
-	database, _ :=
-		sql.Open("sqlite3", "./jkgo.db")
-	statement, _ :=
-		database.Prepare("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT, lastname TEXT)")
-	statement.Exec()
-	statement, _ =
-		database.Prepare("INSERT INTO people (firstname, lastname) VALUES (?, ?)")
-	statement.Exec("Rob", "Gronkowski")
-	rows, _ :=
-		database.Query("SELECT id, firstname, lastname FROM people")
-	var id int
-	var firstname string
-	var lastname string
-	for rows.Next() {
-		rows.Scan(&id, &firstname, &lastname)
-		fmt.Println(strconv.Itoa(id) + ": " + firstname + " " + lastname)
-	}
+	myChichitos := createChichitosList("MyListaDeChichis")
+	fmt.Println("Se ha creado una lista vacia de Chichitos!")
+
+	myChichitos.addChichis("BB", "El bebito", 20)
+	myChichitos.addChichis("Bichito", "El bichito de Luz", 16)
+	myChichitos.addChichis("Chichiiii", "Chiichiiiiiii", 10)
+	myChichitos.addChichis("Sergio", "Sergio Mauricio", 0)
+	myChichitos.showAllChichitos()
+
+	myChichitos.deleteChichis("Sergio", "Sergio Mauricio", 0)
+	myChichitos.deleteChichis("Bichito", "El bichito de Luz", 16)
+	myChichitos.showAllChichitos()
+
+	myChichitos.addChichis("Gordo", "BB Gordo y Feo", 20)
+	myChichitos.showAllChichitos()
 }
