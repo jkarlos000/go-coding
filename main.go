3b51cf1eb6f32558cee880fe5892bb7fed722d64
jkarlos000 <jc.ulloa@jkarlos.info>
2020-03-29 18:39:46 -0400

    Uso de FOR como While

--- main.go
+++ main.go
@@ -1,29 +1,20 @@
 package main
 
-import "fmt"
-
-const (
-	a = 42
-	b int = 43
+import (
+	"fmt"
+	"time"
 )
 
 func main() {
-	x := 42
-	fmt.Printf("%d\t\t%b\t\t%#x\n",x,x,x)
-
-	fmt.Println(42 == 42)
-	fmt.Println(43 <= 43)
-	fmt.Println(42>=41)
-	fmt.Println(42 != 43)
-	fmt.Println(42 < 43)
-	fmt.Println(42>41)
-
-	fmt.Println(a,b)
-
-	y := 42
-	fmt.Printf("%d\t%b\t%#x\n", y,y,y)
-	z := y << 1
-	z = z | 1
-	fmt.Printf("%d\t%b\t%#x\n", z,z,z)
-
+	x := 0
+	for {
+		x ++
+		if x%2 != 0 {
+			fmt.Println("Hehehehe, sopenco, tu sos",x)
+			//fmt.Printf("Hehehehe, sopenco, tu sos %v", x)
+			continue
+		}
+		fmt.Println(x)
+		time.Sleep(time.Second*1)
+	}
 }
