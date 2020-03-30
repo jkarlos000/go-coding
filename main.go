e042353dda112cdff386603e55019e0447f9288a
jkarlos000 <jc.ulloa@jkarlos.info>
2020-03-29 22:19:58 -0400

    Go Uso de sentencias como parametros

--- main.go
+++ main.go
@@ -3,18 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	for i := 0; i <= 122; i++ {
-		fmt.Printf("%d\t%#x\t%#U\n",i,i,i)
-		switch {
-		case i%2 == 0, i > 120://Cualquiera de estas condiciones sea verdadero, ejecutara la instrucción
-			fmt.Println(i)
-		case i == 15:
-			fmt.Println("Soy 15")
-			fallthrough
-		case i == 2:
-			fmt.Println("SOy 2 y 15 luego")
-		default:
-			fmt.Println("Default")
-		}
+	for i := 10; i <= 100; i++ {
+		fmt.Printf("Cuando dividimos %v entre 4, el resto (también módulo) es %v\n", i, i%4)
 	}
 }
