329eb073bc4c26b3faa15991853cb716f25d7d1d
jkarlos000 <jc.ulloa@jkarlos.info>
2020-03-30 21:53:29 -0400

    GO Punteros, Struct & Interfaces, usar correctamente GOLAND para auto-implementar metodos

--- main.go
+++ main.go
@@ -2,23 +2,47 @@ package main
 
 import "fmt"
 
-func main() {
-	g := func(xi []int) int {
-		if len(xi) == 0 {
-			return 0
-		}
-		if len(xi) == 1 {
-			return xi[0]
-		}
-		return xi[0] + xi[len(xi) - 1]
-	}
-
-	x := foo(g, []int{1,2,3,4,5})
-	fmt.Println(x)
+type Person struct {
+	Name, LastName string
+	Age int
 }
 
-func foo(f func([]int) int, ii []int) int {
-	n := f(ii)
-	n++
-	return n
+type Human interface {
+	getName()
+	getLastName()
+	getFullName()
+	getAge()
+	setFullName(name string, lastname string)
+}
+
+func (p Person) getName() {
+	panic("implement me")
+}
+
+func (p Person) getLastName() {
+	panic("implement me")
+}
+
+func (p Person) getFullName() {
+	panic("implement me")
+}
+
+func (p Person) getAge() {
+	panic("implement me")
+}
+
+func (p *Person) setFullName(name string, lastname string) {
+	p.Name = name
+	p.LastName = lastname
+}
+
+func main() {
+	p := &Person{
+		Name:     "Juan Carlos",
+		LastName: "Ulloa Vasquez",
+		Age:      25,
+	}
+	fmt.Printf("%+v", *p)
+	p.setFullName("Jose Eduardo", "Ulloa Pam")
+	fmt.Printf("%+v", *p)
 }
