35c86a42751a97961853ea5c3fbf1e883fb40f70
jkarlos000 <jc.ulloa@jkarlos.info>
2020-03-31 19:03:17 -0400

    GO Runtime, multiproceso, mejorando el Raise Condition mediante Sync.Atomic, este paquete de sincronización mejora el uso de concurrencia eliminando el RaiseCondition

--- main.go
+++ main.go
@@ -4,25 +4,23 @@ import (
 	"fmt"
 	"runtime"
 	"sync"
+	"sync/atomic"
 )
 
 func main() {
 	fmt.Println("Número de CPU:", runtime.NumCPU())
 	fmt.Println("Número de Go-Runtime:", runtime.NumGoroutine())
-	contador := 0
+	runtime.GOMAXPROCS(8) //Cantidad de CPU a usar (mi maquina soporta 8)
+	var contador int64
 
-	const gs = 100
+	const gs = 99999
 	var wg sync.WaitGroup
 	wg.Add(gs)
-	var mu sync.Mutex
 	for i := 0; i < gs; i++ {
 		go func() {
-			mu.Lock()
-			v := contador
-			v++
+			atomic.AddInt64(&contador, 1)
 			runtime.Gosched()
-			contador = v
-			mu.Unlock()
+			fmt.Println("Contador:", atomic.LoadInt64(&contador))
 			wg.Done()
 		}()
 		fmt.Println("Número de Go-Runtime:", runtime.NumGoroutine())
