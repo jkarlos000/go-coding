c48297b70bf1315b4751f550fb9ef0cbadf6ad70
jkarlos000 <jc.ulloa@jkarlos.info>
2020-03-31 20:26:25 -0400

    GO, usando paquetes practicamente, Publico la primera letra en mayúsculas

---
+++ gato/felino.go
@@ -0,0 +1,11 @@
+package gato
+
+import "fmt"
+
+func Maullar() {
+	fmt.Println(`El gato maula toda la noche, "Nyaaannn~~~, Nyaaannnn~~~"`)
+}
+
+func Tragar()  {
+	fmt.Println("Esta gorda traga muchas croquetas!")
+}
--- main.go
+++ main.go
@@ -1,31 +1,12 @@
 package main
 
 import (
+	"awesomeProject/gato"
 	"fmt"
-	"runtime"
-	"sync"
-	"sync/atomic"
 )
 
 func main() {
-	fmt.Println("Número de CPU:", runtime.NumCPU())
-	fmt.Println("Número de Go-Runtime:", runtime.NumGoroutine())
-	runtime.GOMAXPROCS(8) //Cantidad de CPU a usar (mi maquina soporta 8)
-	var contador int64
-
-	const gs = 99999
-	var wg sync.WaitGroup
-	wg.Add(gs)
-	for i := 0; i < gs; i++ {
-		go func() {
-			atomic.AddInt64(&contador, 1)
-			runtime.Gosched()
-			fmt.Println("Contador:", atomic.LoadInt64(&contador))
-			wg.Done()
-		}()
-		fmt.Println("Número de Go-Runtime:", runtime.NumGoroutine())
-	}
-	wg.Wait()
-	fmt.Println("Cuenta:", contador)
-	//Compilación cruzada: GOOS=linux GOARCH=amd64 go build main.go
+	fmt.Println("Llamando a mi paquete gato")
+	gato.Maullar()
+	gato.Tragar()
 }
