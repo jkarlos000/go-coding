02ba9c40d9f0fd8d6bf47d8ce59f2d4d4c19a9a9
jkarlos000 <jc.ulloa@jkarlos.info>
2020-03-31 23:53:46 -0400

    GO Canales uso de range y cierre de canales para evitar deadlock

--- main.go
+++ main.go
@@ -4,19 +4,18 @@ import "fmt"
 
 func main() {
 	c := make(chan int)
-
-	// enviar
-	go enviar(c)
-	/*go*/ recibir(c)
+	//Enviar
+	go func() {
+		for i := 0; i < 5; i++ {
+			c <- i
+		}
+		close(c)
+	}()
+	//Recibir
+	for v := range c{
+		fmt.Println(v)
+	}
 
 	fmt.Println("Finalizado")
 
-}
-
-func enviar(c chan<- int) {// Solo se recibe un canal SEND ONLY
-	c <- 42
-}
-
-func recibir(c <-chan int) {// SOLO se recibe un canal RECEIVE ONLY
-	fmt.Println(<-c)
 }
