91d341270cf7558bc5a74d29cc87a9084913e64a
jkarlos000 <jc.ulloa@jkarlos.info>
2020-04-02 00:39:24 -0400

    Revisar el uso de GoRuntime, con canales, la cantidad de canales debenvaciarse antes de cerrar main

--- main.go
+++ main.go
@@ -1,45 +1,21 @@
 package main
 
-import (
-	"context"
-	"fmt"
-	"runtime"
-	"time"
-)
+import "fmt"
 
 func main() {
-	runtime.GOMAXPROCS(8)
-	ctx, cancel := context.WithCancel(context.Background())
+	c := make(chan int)
 
-	fmt.Println("Verificando el error 1:", ctx.Err())
-	fmt.Println("Número de Go Runtime 1:", runtime.NumGoroutine())
-
-	go func() {
-		n := 0
-		for {
-			select {
-			case <- ctx.Done():
-				return
-			default:
-				n++
-				time.Sleep(time.Millisecond * 200)
-				fmt.Println("Trabajo muy duro, como un esclavo, denme dinero +", n)
+	for j:= 0 ; j<10; j++ {
+		go func() {
+			for i := 0; i < 10; i++ {
+				c <- i
 			}
-		}
-	}()
-	time.Sleep(time.Second*2)
+		}()
+	}
+	//close(c)
 
-	fmt.Println("Verificando error:", ctx.Err())
-	fmt.Println("Número de GORuntime 2da:", runtime.NumGoroutine())
-
-	fmt.Println("A punto de cancelar el context")
-
-	cancel()
-
-	fmt.Println("Context cancelado")
-
-	time.Sleep(time.Second*2)
-	fmt.Println("Verificando error 3:", ctx.Err())
-	fmt.Println("Número de GORuntime 3ra:", runtime.NumGoroutine())
+	for k := 0; k< 100; k++ {
+		fmt.Println(k, <- c)
+	}
 
 }
