cda7af1c1d848204607a540620dfb0d061e9c38c
jkarlos000 <jc.ulloa@jkarlos.info>
2020-04-07 18:52:07 -0400

    Go testing con Tablas de pruebas

--- main_test.go
+++ main_test.go
@@ -3,8 +3,32 @@ package main
 import "testing"
 
 func TestMiSuma(t *testing.T) {
-	v := miSuma(2,8)
-	if v != 10 {
-		t.Error("Expected", 10, "Got", miSuma(2,8))
+	type prueba struct {
+		datos []int
+		respuesta int
+	}
+	pruebas := []prueba{
+		prueba{
+			datos:     []int{2, 4, 6},
+			respuesta: 12,
+		},
+		prueba{
+			datos:     []int{1, 5, 2},
+			respuesta: 8,
+		},
+		prueba{
+			datos:     []int{0, -1, 1},
+			respuesta: 0,
+		},
+		prueba{
+			datos:     []int{-10, 4, 20},
+			respuesta: 14,
+		},
+	}
+	for _, x := range pruebas{
+		v := miSuma(x.datos...)
+		if v != x.respuesta {
+			t.Error("Expected", x.respuesta, "Got", v)
+		}
 	}
 }
